ea2c22c31302a50b4a10675e9373898e311686b4
Mohsin <mohsin.awan0@gmail.com>
2023-05-13 13:33:04 +0500

    Backend repo deleted

--- backend/src/routes/stripe.js
+++
@@ -1,9 +0,0 @@
-const stripeController = require('../controllers/stripe');
-
-const authorize = require('../middlewares/authorize');
-module.exports = (app) => {
-    app.route('/session')
-        .post(authorize, stripeController.session)
-    app.route('/webhook')
-        .post(stripeController.webhook)
-}
