a8f8a857c30e698ecd9217627e66a71f119c4c04
reyan1243 <65946237+reyan1243@users.noreply.github.com>
2023-06-07 16:12:10 +0500

    client updated

--- frontend/src/service/client.js
+++ frontend/src/service/client.js
@@ -1,18 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 const client = axios.create({
-  // baseURL: 'https://3.23.244.114:3000/',
-  baseURL: 'http://localhost:4000/',
+  baseURL: "https://3.23.244.114:3000/",
+  // baseURL: 'http://localhost:4000/',
   headers: {
-    "auth-token" : `${localStorage.getItem("token")}`
-  }
+    "auth-token": `${localStorage.getItem("token")}`,
+  },
 });
 
-client.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  config.headers['auth-token'] = `${token}`;
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+client.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    config.headers["auth-token"] = `${token}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export { client };
