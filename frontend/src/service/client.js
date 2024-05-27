5b8e7c23e0ca384efdf895ccabc5be8a5f55f31e
Muneeb Ur Rehman <mb380960@gmail.com>
2024-05-27 20:50:43 +0500

    baseurl changed

--- frontend/src/service/client.js
+++ frontend/src/service/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const client = axios.create({
-  baseURL: "https://3.23.244.114:3000/",
-  // baseURL: 'http://localhost:4000/',
+  baseURL: "https://3.16.225.196:3000/",
+  // baseURL: "http://localhost:4000/",
   headers: {
     "auth-token": `${localStorage.getItem("token")}`,
   },
